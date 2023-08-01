c4483d312b9b9ae3fe1029b83a6ed3c5980fea70
César Cláudio Cattermol <cc.catter@gmail.com>
2023-08-01 10:00:37 -0300

    exercicios  e 2 prontos

---
+++ exercicio01.py
@@ -0,0 +1,17 @@
+'''
+Elabore um programa que pergunte ao aluno suas 3 notas escolares.
+O programa deverá calcular a média das 3 notas inseridas e
+exibir esta medida
+'''
+
+# Etapa 1: entrada de dados
+nota1 = float(input("informe a  sua primeira nota "))
+nota2 = float(input("informe a  sua segunda nota"))
+nota3 = float(input("informe a  sua terceira nota"))
+
+# Etapa 2: processamento de dados
+media =  (nota1 + nota2 + nota3) /3
+
+# Etapa 3: Saída de sados ()resposta na tela do usuário
+print("sua média é: ", media)
+
---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+'''
+Fazer um programa que pergunte um número intero e apresente ao usuário o antecessor e o sucessor do número informado.
+'''
+
+numero = int(input("informe um número inteiro"))
+
+ant = numero-1
+print("o antecessor de",numero ,"é", ant)
+
+sus = numero + 1
+print("o sucessor de",numero, "é", sus)
